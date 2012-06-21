4724385e9039f20b94edd47377ecf5dc8a9668fb
Runar Myklebust <rmy@enonic.com>
2012-06-21 10:50:28 +0200

    Added parameterized index-name for load.sh

--- loadData.sh
+++ loadData.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "argument 'indexName' required, eg: ./loadData.sh goRunarIndex"
+
 echo "Deleting old index"
-curl -XDELETE 'http://localhost:9200/test/'
+curl -XDELETE 'http://leela:9200/$1/'
 
 FILES=./timeLogDataImport_*
 
 for f in $FILES
 do
   echo "Processing file: $f"
-  curl -s -XPUT 'http://localhost:9200/_bulk' --data-binary @$f
+  curl -s -XPUT 'http://leela:9200/_bulk' --data-binary @$f
 done
 
 
